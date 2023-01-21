485c1fdca2d198adc5c60758eab1a8bfef6e9f0b
ullasasindhur <ullasa.sindhur@gmail.com>
2023-01-21 23:18:24 +0530

    Moved from aws-sdk to client-s3

--- server.js
+++ server.js
@@ -3,39 +3,37 @@ const process = require("process");
 const fs = require("fs");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
+require("dotenv").config();
 
-const aws = require("aws-sdk");
-aws.config.credentials = new aws.SharedIniFileCredentials();
-if (aws.config.credentials.accessKeyId == undefined) {
-  console.error("Please configure AWS CLI on your system");
-  process.exit();
-} else {
-  console.log("Successfully loaded AWS Credentials");
-}
-const s3 = new aws.S3();
-
-const port = 3000;
+const {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+} = require("@aws-sdk/client-s3");
+const s3Client = new S3Client({
+  region: process.env.REGION,
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  },
+});
+const bucket = process.env.BUCKET_NAME;
 const allowedExtensions = ["pdf", "xlsx", "xls"];
 
+const port = 3000;
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((error, req, res, next) => {
-  console.log("This is the rejected field ->", error.field);
-});
-
 app.get("/", (req, res) => {
   res.status(200).json({ Message: "Welcome to s3API" });
 });
 
-app.post("/upload", upload.single("key"), (req, res) => {
-  console.log(req.file);
+app.post("/upload", upload.single("key"), async (req, res) => {
   if (req.file == undefined) {
     return res.status(400).send({ Message: "Missing file to upload" });
   }
-  const bucket = "ullasasindhur";
   const key = req.file.originalname;
   if (allowedExtensions.includes(key.split(".").slice(-1)[0])) {
     const options = {
@@ -43,15 +41,16 @@ app.post("/upload", upload.single("key"), (req, res) => {
       Bucket: bucket,
       Key: key,
     };
-    s3.upload(options, (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        return res
-          .status(201)
-          .json({ Message: `File ${data.key} uploaded successfully` });
-      }
-    });
+    try {
+      await s3Client.send(new PutObjectCommand(options));
+      return res
+        .status(201)
+        .json({ Message: `File ${options.Key} uploaded successfully` });
+    } catch (err) {
+      return res
+        .status(err.$metadata.httpStatusCode)
+        .send({ Message: err.Code });
+    }
   } else {
     return res.status(501).send({
       Message: "Sorry, currently we are supporting only PDF/Excel files",
@@ -59,25 +58,25 @@ app.post("/upload", upload.single("key"), (req, res) => {
   }
 });
 
-app.get("/download", (req, res) => {
+app.get("/download", async (req, res) => {
   const key = req.query["key"];
   if (key == undefined) {
     return res.status(400).send({ Message: "Missing key parameter" });
   }
-  const bucket = "ullasasindhur";
   if (allowedExtensions.includes(key.split(".").slice(-1)[0])) {
     const options = {
       Bucket: bucket,
       Key: key,
     };
-    s3.getObject(options, (err, data) => {
-      if (err) {
-        return res.status(err.statusCode).send({ Message: err.message });
-      } else {
-        res.attachment(key);
-        return res.send(data.Body);
-      }
-    });
+    try {
+      const data = await s3Client.send(new GetObjectCommand(options));
+      res.attachment(key);
+      res.send(Buffer.concat(await data.Body.toArray()));
+    } catch (err) {
+      return res
+        .status(err.$metadata.httpStatusCode)
+        .send({ Message: err.Code });
+    }
   } else {
     return res.status(501).send({
       Message: "Sorry, currently we are supporting only PDF/Excel files",
